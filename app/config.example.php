2f4bb275c2e8421e49bf05d9216e12052fb888df
kasherehauwa01-sudo <kasherehauwa01@gmail.com>
2026-06-04 16:40:48 +0300

    Update config.example.php

--- app/config.example.php
+++ app/config.example.php
@@ -4,11 +4,11 @@
  */
 return [
     // URL опубликованного Google Apps Script Web App (/exec).
-    'gas_web_app_url' => 'https://script.google.com/macros/s/PASTE_DEPLOYMENT_ID/exec',
+    'gas_web_app_url' => 'https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbwSULl7K300c6yLjDm8BZ1LhVc_FvoBUsggEX5lp9fQofEko9ZCDXgYquQKGKKvbMGp/exec',
 
     // Общий секрет для защиты обмена между сайтом и Google Apps Script.
     // Такое же значение нужно указать в Script Properties с ключом API_SECRET.
-    'api_secret' => 'change-me-long-random-secret',
+    'api_secret' => 'AKfycbwSULl7K300c6yLjDm8BZ1LhVc_FvoBUsggEX5lp9fQofEko9ZCDXgYquQKGKKvbMGp',
 
     // Таймаут запросов к Apps Script в секундах.
     'request_timeout' => 30,
