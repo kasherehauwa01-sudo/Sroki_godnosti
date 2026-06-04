74a0c138c304e8950625eee92e53eea4a8ac786c
kasherehauwa01-sudo <kasherehauwa01@gmail.com>
2026-06-04 17:45:35 +0300

    Update config.example.php

--- app/config.example.php
+++ app/config.example.php
@@ -4,7 +4,7 @@
  */
 return [
     // URL опубликованного Google Apps Script Web App (/exec).
-    'gas_web_app_url' => 'https://script.google.com/macros/s/AKfycbwqsa4WR-iAqTKyrUynEDVKjwrv9H9faqvVc3ClppP95VxuQOoMhE75KT4iQkysbxM6/exec',
+    'gas_web_app_url' => 'https://script.google.com/macros/s/AKfycbwqsa4WR-iAqTKyrUyEDIVKjwrv9H9faqyVc3lppP95VxuQ0oMheI75KT4iQloyxbsM6/exec',
 
     // Общий секрет для защиты обмена между сайтом и Google Apps Script.
     // Такое же значение нужно указать в Script Properties с ключом API_SECRET.
