14663e3cb0bc04db56d8e7644b8aee0539933ac3
kasherehauwa01-sudo <kasherehauwa01@gmail.com>
2026-08-05 14:45:03 +0300

    Detect nested catalogvr stock data

--- tests/vrcatalog_manager_fallback_test.php
+++ tests/vrcatalog_manager_fallback_test.php
@@ -40,6 +40,21 @@ assertSameValue(
     'Форма склада 2 должна содержать только товары с положительным остатком'
 );
 
+$russianStockProduct = ['article' => '346051', 'found' => true, 'stocks' => [
+    ['Склад' => 'Бахтурова', 'Остаток' => 2],
+    ['Склад' => 'Авиаторов Зал+Склад', 'Остаток' => 13],
+]];
+assertSameValue(2.0, vrCatalogWarehouseStockQuantity($russianStockProduct, ['name' => 'Бахтурова']), 'Остаток должен читаться из русских ключей catalogvr');
+assertSameValue(13.0, vrCatalogWarehouseStockQuantity($russianStockProduct, ['name' => 'Авиаторов Зал']), 'Объединённый склад catalogvr должен сопоставляться с залом');
+assertSameValue(13.0, vrCatalogWarehouseStockQuantity($russianStockProduct, ['name' => 'Авиаторов Склад']), 'Объединённый склад catalogvr должен сопоставляться со складом');
+
+$nestedStockProduct = ['article' => '346051', 'found' => true, 'data' => ['remains' => [
+    ['Склад' => 'Козловская', 'Остаток' => '3 шт.'],
+    ['Название склада' => 'Стройград', 'Количество' => '7,5'],
+]]];
+assertSameValue(3.0, vrCatalogWarehouseStockQuantity($nestedStockProduct, ['name' => 'Козловская']), 'Остаток должен находиться рекурсивно и читаться из строки');
+assertSameValue(7.5, vrCatalogWarehouseStockQuantity($nestedStockProduct, ['name' => 'Стройград']), 'Дробный остаток с запятой должен читаться как число');
+
 foreach ([
     'ЖС2344-1' => 'ЖС2344',
     'ЖС2344-1-25' => 'ЖС2344',
